acb1158e281b38bcc5d584fa58c1ff7c8a3c5287
leavelet <leavelet@163.com>
2023-03-27 23:37:32 +0800

    fix: fix chat bug

--- src/openai/chat.rs
+++ src/openai/chat.rs
@@ -3,6 +3,7 @@ use ::openai::{
     chat::{ChatCompletion, ChatCompletionMessage, ChatCompletionMessageRole},
     set_key,
 };
+use openai::completions::Completion;
 use serde_json::json;
 
 pub(crate) async fn chat(chat_config: &Chat, prompt: &str) -> serde_json::Value {
@@ -10,59 +11,26 @@ pub(crate) async fn chat(chat_config: &Chat, prompt: &str) -> serde_json::Value 
 
     let mut messages = vec![ChatCompletionMessage {
         role: ChatCompletionMessageRole::System,
-        content: "You are a large language model built into a command line interface for PKU software course homework. Please speak in English only".to_string(),
+        content: "You are a large language model built into a command line interface as an example of what the `openai` Rust library made by Valentine Briese can do.".to_string(),
         name: None,
     }];
 
-    let prompt = prompt.to_string();
-
     messages.push(ChatCompletionMessage {
         role: ChatCompletionMessageRole::User,
-        content: prompt,
+        content: prompt.to_string(),
         name: None,
     });
 
     let chat_completion = ChatCompletion::builder("gpt-3.5-turbo", messages.clone())
         .create()
-        .await;
+        .await
+        .unwrap()
+        .unwrap();
 
-    let chat_completion = match chat_completion {
-        Ok(chat_completion) => chat_completion,
-        Err(err) => {
-            error!("Failed to get chat completion: {}", err);
-            return serde_json::json!({
-                "status": "failed",
-                "error": "Network error",
-            });
-        }
-    };
-
-    let chat_completion = match chat_completion {
-        Ok(chat_completion) => chat_completion,
-        Err(err) => {
-            error!("OpenAI error: {}", err);
-            return serde_json::json!({
-                "status": "failed",
-                "error": "OpenAI error",
-            });
-        }
-    };
-
-    let returned_message = chat_completion.choices.first();
-    let returned_message = match returned_message {
-        Some(returned_message) => returned_message,
-        None => {
-            error!("No returned message");
-            return serde_json::json!({
-                "status": "failed",
-                "error": "No returned message",
-            });
-        }
-    };
-    let returned_message = returned_message.message.clone();
+    let returned_message = chat_completion.choices.first().unwrap().message.clone();
 
     json!({
         "status": "ok",
-        "text": returned_message,
+        "text": returned_message.content.trim(),
     })
 }
